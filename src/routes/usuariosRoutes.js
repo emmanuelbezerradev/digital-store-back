d6b826d33372383629b7bf5485005d2417dded89
emmanuelbezerradev <emmanuelbezerra1992@gmail.com>
2025-05-28 17:11:47 -0300

    teste

---
+++ src/routes/usuariosRoutes.js
@@ -0,0 +1,25 @@
+const { executarSQL } = require("../services");
+
+const router = require("express").Router();
+
+router.get("/", async (req, res) => {
+    res.send( await executarSQL("SELECT * FROM produtos;"));
+});
+
+router.get("/:id", (req, res) => {
+    res.send(`Busca um usuario com o id: ${req.params.id}`);
+});
+
+router.post("/", (req, res) => {
+    res.send("Cria um usuario");
+});
+
+router.put("/:id", (req, res) => {
+    res.send(`Edita um usuario com oo id: ${req.params.id}`);
+});
+
+router.delete("/:id", (req, res) => {
+    res.send(`Apaga um usuario com o id: ${req.params.id}`);
+});
+
+module.exports = router;
---
+++ src/services/index.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql2/promise");
+
+async function executarSQL(comandoSql){
+    const conexao = await mysql.createConnection({
+        host:"localhost",
+        user:"root",
+        password:"1234",
+        port:"3306",
+        database:"digital_store_db",
+    });
+    const [result] = await conexao.query(comandoSql);
+    conexao.end();
+
+    return result;
+}
+
+module.exports = {executarSQL};
