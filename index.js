4161156dc17f58a4836dd3524706509f035c5a64
emmanuelbezerradev <emmanuelbezerra1992@gmail.com>
2025-05-29 16:58:37 -0300

    prisma

--- index.js
+++ index.js
@@ -5,29 +5,20 @@ const port = 8000;
 const usuariosRoutes = require("./src/routes/usuariosRoutes");
 const produtosRoutes = require("./src/routes/produtosRoutes");
 
-app.get("/,",(red, res) => {
+app.use(express.json());
+
+app.get("/,",(req, res) => {
     res.send("produtos");
 });
+
+app.use("/usuarios", usuariosRoutes);
 app.use("/produtos", produtosRoutes);
+
+
 app.use((req, res) => {
     res.status(404).send("Rota não encotnrada");
 });
 app.listen(port,() => {
     console.log(`Servidor de pé:http://localhost:${port}`);
     
-});
-
-// USUARIOS
-app.get("/", (req, res) => {
-    res.send("olá mundo");
-});
-
-app.use("/usuarios", usuariosRoutes);
-
-app.use((req,res) => {
-    res.status(404).send("Rota não encontrada");
-});
-
-app.listen(port, () => {
-    console.log(`Servidor de pé: http://localhost:${port}`);    
 });
--- src/routes/produtosRoutes.js
+++ src/routes/produtosRoutes.js
@@ -1,23 +1,31 @@
-const { executarSQL } = require("../services");
+const { buscarProdutos, buscarUmProduto, criarProduto, editarProduto, apagarProduto } = require("../controller/produtosController");
 
 const router = require("express").Router();
 
 router.get("/", async (req, res) => {
-    res.send( await executarSQL("SELECT * FROM produtos;"));
+    res.send( await buscarProdutos() );
 });
 
-router.get("/:id", (req, res) => {
-    res.send(`Busca um usuario com o id: ${req.params.id}`);
+router.get("/:id", async (req, res) => {
+    res.send( await buscarUmProduto(req.params.id));
 });
 
-router.post("/", (req, res) => {
-    res.send("Cria um usuario");
+router.post("/", async (req, res) => {
+    res.send(await criarProduto(req.body));
 });
 
+// router.put("/:id", (req, res) => {
+//     res.send(`Edita um produto com o id: ${req.params.id}`);
+// });
+router.put("/:id", async (req, res) => {
+    res.send(await editarProduto(req.params.id, req.body));
+});
 router.put("/:id", (req, res) => {
-    res.send(`Edita um produto com oo id: ${req.params.id}`);
+    res.send(`Edita um produto com o id: ${req.params.id}`);
 });
-
+router.delete("/:id", async (req,res) => {
+    res.send(await apagarProduto(req.params.id));
+})
 router.delete("/:id", (req, res) => {
     res.send(`Apaga um produto com o id: ${req.params.id}`);
 });
