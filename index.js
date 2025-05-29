5eb967ca46cc520b698517f45955ffd93a53c3e5
Emmanuel Bezerra <emmanuelbezerra1992@gmail.com>
2025-05-28 22:02:43 -0300

    casa done

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
 const port = 8000;
-
+// PRODUTOS
 const usuariosRoutes = require("./src/routes/usuariosRoutes");
+const produtosRoutes = require("./src/routes/produtosRoutes");
 
+app.get("/,",(red, res) => {
+    res.send("produtos");
+});
+app.use("/produtos", produtosRoutes);
+app.use((req, res) => {
+    res.status(404).send("Rota não encotnrada");
+});
+app.listen(port,() => {
+    console.log(`Servidor de pé:http://localhost:${port}`);
+    
+});
+
+// USUARIOS
 app.get("/", (req, res) => {
     res.send("olá mundo");
 });
